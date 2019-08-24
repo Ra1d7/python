88f68fe79534dc16fc10e7fcbca687e3bd85d315
Raid7 <thmrgb8@gmail.com>
2019-08-24 15:37:02 +0300

    Add files via upload

---
+++ LolResults.py
@@ -0,0 +1,23 @@
+path = ''
+def dothisshit():
+    global path
+    try:
+        f = open(path)
+        print('\033[31m\033[1mFile Opened..\033[0m\n')
+        for line in f.readlines():
+            secs = line.split('|')
+            rpnum = secs[3].replace("RP = ",'')
+            rpnum = rpnum.replace(' ','')
+            if int(rpnum) > 120:
+                print('\033[32m {}\033[0m'.format(line))
+    except:
+        print('\033[1;91mINVALID PATH\033[0m')
+        main()
+def main():
+    global path
+    path = input('Path of File: ')
+    print('Trying To Open File: {}'.format(path))
+    dothisshit()
+print('\033[33mLeague of legends results Sorter for LolSamurai config')
+print('Made by Raid7\033[0m')
+main()
